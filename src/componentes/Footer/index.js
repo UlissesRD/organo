49ce3cdc5a791cfb70e1266936f76e827edaf9ff
UlissesRD <pedroulisses@advocareis.com.br>
2024-05-20 14:16:17 -0300

    The footer is born!

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,35 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (<footer className="footer">
+        <section>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/pedro-ulisses-reis-de-dorneles-6047a626b" target="_blank">
+                        <img src="/imagens/li.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/UlissesRD" target="_blank">
+                        <img src="/imagens/gh.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/pedro_ulissesrd/" target="_blank">
+                        <img src="/imagens/ig.png" alt="" />
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section className='logo'>
+            <img src="/imagens/logo.png" alt="" />
+        </section>
+        <section>
+            <p>
+                Developed by Pedro Ulisses.
+            </p>
+        </section>
+    </footer>)
+}
+
+export default Footer
